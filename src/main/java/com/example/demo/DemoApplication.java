0bdb6e07b7a70f75e0ee4842fac2e78544125d24
Alexis Plantin <alexis.plantin@gmail.com>
2023-04-05 15:01:21 +0200

    Add passwordless configuration

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,6 +2,12 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.stream.Stream;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -9,5 +15,12 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
+
+	 @Bean
+    ApplicationListener<ApplicationReadyEvent> basicsApplicationListener(TodoRepository repository) {
+        return event->repository
+            .saveAll(Stream.of("A", "B", "C").map(name->new Todo("configuration", "congratulations, you have set up correctly!", true)).toList())
+            .forEach(System.out::println);
+    }
 
 }
